e3a624a79c3001ed4dbaa0e230175ac050db426a
makis <54209831+thioaana@users.noreply.github.com>
2020-07-01 11:35:59 +0300

    1_1 Points And Vector completed

--- PointInclusionInAPolygonWeek1/1_1PointsAndVector.py
+++ PointInclusionInAPolygonWeek1/1_1PointsAndVector.py
@@ -1,6 +1,9 @@
+#uses python3
+
 import numpy as np
-import matplotlib.pyplot as plt
-import sys
+# import matplotlib.pyplot as plt
+# import sys
+
 def getInput():
     [aX, aY, bX, bY] = [int(i) for i in input().split()]
     a = (aX, aY)
@@ -8,63 +11,41 @@ def getInput():
     n = int(input())
     p = []
     for i in range(n):
-        p.append((int(j), for j in imput().split()))
+        temp = input().split()
+        p.append((int(temp[0]), int(temp[1])))
     return a, b, p
 
-def PointAndVector(v0, v1, myPoint):
-    # Guaranteed that the vector has non - zero length.
-    if v0[1] < v1[1] :
-        vStart = v0; vEnd = v1
-    else v0[1] > v1[1] :
-        vStart = v1; vEnd = v0
-    # else :
-    #     print("The vector has 0 length - Program aborts")
-    #     sys.exit()
-
+def PointAndVector(vStart, vEnd, myPoint):
     a = np.array([vStart, vEnd, myPoint])
     b = np.ones((3, 1))
-    d = np.hstack((a, b)) # append a column
-    if np.linalg.det(d) > 0 :
+    d = np.hstack((a, b))  # append a column
+    if np.linalg.det(d) > 0:
         return "LEFT"
     elif np.linalg.det(d) < 0:
         return "RIGHT"
-    else :
-        if vStart[1] != vEnd[1] # In case of non - horizontal vector
-            if p[1] < vStart[1] or p[1] > vEnd[1] :
-                return "ON LINE"
-            else :
-                return "ON SEGMENT"
-        else :  # In case of horizontal vector
-            if p[0] < vStart[0] or p[0] > vEnd[0] :
-                return "ON LINE"
-            else :
-                return "ON SEGMENT"
+    else:
+        maxVertical = max(vStart[1], vEnd[1])
+        minVertical = min(vStart[1], vEnd[1])
+        maxHorizontal = max(vStart[0], vEnd[0])
+        minHorizontal = min(vStart[0], vEnd[0])
+        if vStart[1] != vEnd[1] : # In case of non - horizontal vector
+            if p[1] < minVertical or p[1] > maxVertical:
+                return "ON_LINE"
+            else:
+                return "ON_SEGMENT"
+        else:  # In case of horizontal vector
+            if p[0] < minHorizontal or p[0] > maxHorizontal:
+                return "ON_LINE"
+            else:
+                return "ON_SEGMENT"
 
 if __name__ == '__main__':
     A, B, points = getInput()
     for p in points:
         print(PointAndVector(A, B, p))
 
-
-    userInput = input("Enter coords of A and B with space between :").split()
-    A = (int(userInput[0]), int(userInput[1]))
-    B = (int(userInput[2]), int(userInput[3]))
-
-
-    n = 0
-    while n < 1 or n > 1000 :
-        n = int(input("Enter the number of points : "))
-
-    points = []
-    for i in range(n):
-        userInput = input("Enter coords of a point with space between :").split()
-        (x, y) = (int(userInput[0]), int(userInput[1]))
-        points.append((x, y))
-
-    for p in points:
-        print(PointAndVector(A, B, p))
-
-    for p in points:
-        plt.scatter(p[0], p[1])
-    plt.plot([A[0], B[0]], [A[1], B[1]])
-    plt.show()
+    # Plotting - For testing reasons
+    # for p in points:
+    #     plt.scatter(p[0], p[1])
+    # plt.plot([A[0], B[0]], [A[1], B[1]])
+    # plt.show()
